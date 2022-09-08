b9156204db0e7b1ee85d8c5066933120be4629f5
Tiger <ramintiger123@gmail.com>
2022-09-08 15:27:21 +0700

    done totalpages

--- totalPages.js
+++ totalPages.js
@@ -1,6 +1,12 @@
 const { template } = require('@babel/core')
+// - กรณี arrayItems เป็น null หรือ undefined ให้ return undefined
+// - กรณี rowsPerPage เป็น null หรือ undefined หรือเป็น 0 จะ return 1
 
 function totalPages(arrayItems, rowsPerPage) {
-  //code here
+  if (arrayItems === null || arrayItems === undefined){
+  return undefined 
+  }else if (rowsPerPage === null || rowsPerPage === undefined || rowsPerPage === 0){
+  return 1
+  }else {return Math.ceil(arrayItems.length/rowsPerPage)}
 }
 module.exports = totalPages
